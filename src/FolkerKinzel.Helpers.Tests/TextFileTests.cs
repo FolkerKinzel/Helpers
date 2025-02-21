58110188496bad6fb659c326e00a01796f03a8ac
Folker Kinzel <fkinzel@freenet.de>
2025-02-22 00:25:53 +0100

    tests

---
+++ src/FolkerKinzel.Helpers.Tests/TextFileTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolkerKinzel.Helpers.Tests;
+
+[TestClass]
+public class TextFileTests
+{
+    [NotNull]
+    public TestContext? TestContext { get; set; }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void InitStreamWriterTest1()
+    {
+        using StreamWriter writer = TextFile.OpenWrite(null!, null, null, false);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void InitStreamWriterTest2()
+    {
+        using StreamWriter writer = TextFile.OpenWrite("", null, null, false);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void InitStreamWriterTest3()
+    {
+        using StreamWriter writer = TextFile.OpenWrite("  ", null, null, false);
+    }
+
+    [TestMethod]
+    public void InitStreamWriterTest4()
+    {
+        string path = Path.Combine(TestContext.TestRunResultsDirectory!, "InitStreamWriterTest4.txt");
+        using StreamWriter writer = TextFile.OpenWrite(path, null, null, false);
+        Assert.IsNotNull(writer);
+        Assert.AreEqual(Encoding.UTF8, writer.Encoding);
+        Assert.AreEqual(Environment.NewLine, writer.NewLine);
+    }
+
+    [TestMethod]
+    public void InitStreamWriterTest5()
+    {
+        string path = Path.Combine(TestContext.TestRunResultsDirectory!, "InitStreamWriterTest5.txt");
+        using (StreamWriter writer = TextFile.OpenWrite(path, Encoding.Unicode, "::", false))
+        {
+            Assert.IsNotNull(writer);
+            Assert.AreEqual(Encoding.Unicode, writer.Encoding);
+            Assert.AreEqual("::", writer.NewLine);
+            writer.WriteLine("one");
+        }
+
+        using (StreamWriter writer = TextFile.OpenWrite(path, Encoding.Unicode, "::", true))
+        {
+            writer.Write("two");
+        }
+
+        string text = File.ReadAllText(path, Encoding.Unicode);
+        Assert.AreEqual("one::two", text);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void InitStreamReaderTest1()
+    {
+        using StreamReader reader = TextFile.OpenRead(null!, null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void InitStreamReaderTest2()
+    {
+        using StreamReader reader = TextFile.OpenRead("", null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void InitStreamReaderTest3()
+    {
+        using StreamReader reader = TextFile.OpenRead("  ", null);
+    }
+
+    [TestMethod]
+    public void InitStreamReaderTest4()
+    {
+        using StreamReader reader = TextFile.OpenRead(TestFiles.TestTxt, null);
+        Assert.IsNotNull(reader);
+        Assert.AreEqual(Encoding.UTF8, reader.CurrentEncoding);
+    }
+
+    [TestMethod]
+    public void InitStreamReaderTest5()
+    {
+        using StreamReader reader = TextFile.OpenRead(TestFiles.TestTxt, Encoding.Unicode);
+        Assert.IsNotNull(reader);
+        Assert.AreEqual(Encoding.Unicode, reader.CurrentEncoding);
+    }
+}
---
+++ src/FolkerKinzel.Helpers/TextFile.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+
+namespace FolkerKinzel.Helpers;
+
+/// <summary>
+/// Helper class for operations with text files.
+/// </summary>
+public static class TextFile
+{
+    /// <summary> Opens a text file for reading. </summary>
+    /// <param name="filePath">The file path.</param>
+    /// <param name="textEncoding">The text encoding to be used to read the file,
+    /// or <c>null</c> for <see cref="Encoding.UTF8" />.</param>
+    /// <returns>A <see cref="StreamReader" /> instance for reading the file.</returns>
+    /// <exception cref="ArgumentNullException"> <paramref name="filePath" /> is <c>null</c>.
+    /// </exception>
+    /// <exception cref="ArgumentException"> <paramref name="filePath" /> is not a valid
+    /// file path.</exception>
+    /// <exception cref="IOException">I/O error.</exception>
+    [ExcludeFromCodeCoverage]
+    public static StreamReader OpenRead(string filePath, Encoding? textEncoding)
+    {
+        try
+        {
+            return new StreamReader(filePath, textEncoding ?? Encoding.UTF8, true);
+        }
+        catch (ArgumentNullException)
+        {
+            throw new ArgumentNullException(nameof(filePath));
+        }
+        catch (ArgumentException e)
+        {
+            throw new ArgumentException(e.Message, nameof(filePath), e);
+        }
+        catch (UnauthorizedAccessException e)
+        {
+            throw new IOException(e.Message, e);
+        }
+        catch (NotSupportedException e)
+        {
+            throw new ArgumentException(e.Message, nameof(filePath), e);
+        }
+        catch (System.Security.SecurityException e)
+        {
+            throw new IOException(e.Message, e);
+        }
+        catch (PathTooLongException e)
+        {
+            throw new ArgumentException(e.Message, nameof(filePath), e);
+        }
+        catch (Exception e)
+        {
+            throw new IOException(e.Message, e);
+        }
+    }
+
+    /// <summary> Initializes a new <see cref="StreamWriter" /> instance. </summary>
+    /// <param name="filePath">The file path.</param>
+    /// <param name="textEncoding">The text encoding to be used to write the CSV file
+    /// or <c>null</c> for <see cref="Encoding.UTF8" />.</param>
+    /// <param name="newLine">The line terminator string to be used, or <c>null</c> for 
+    /// <see cref="Environment.NewLine"/>.</param>
+    /// <param name="append">If <c>true</c>, the file is opened for appending; otherwise, 
+    /// the file will be truncated and overwritten. If the specified file does not exist,
+    /// this parameter has no effect, and a new file will be created.
+    /// </param>
+    /// <returns> The newly created <see cref="StreamWriter" /> instance. </returns>
+    /// 
+    /// 
+    /// <exception cref="ArgumentNullException"> <paramref name="filePath" /> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentException"> <paramref name="filePath" /> is not a valid
+    /// file path.</exception>
+    /// <exception cref="IOException">I/O-Error</exception>
+    [ExcludeFromCodeCoverage]
+    public static StreamWriter OpenWrite(string filePath,
+                                         Encoding? textEncoding,
+                                         string? newLine,
+                                         bool append)
+    {
+        try
+        {
+            return new StreamWriter(filePath, append, textEncoding ?? Encoding.UTF8) // UTF-8 with BOM
+            {
+                NewLine = newLine
+            };
+        }
+        catch (ArgumentNullException)
+        {
+            throw new ArgumentNullException(nameof(filePath));
+        }
+        catch (ArgumentException e)
+        {
+            throw new ArgumentException(e.Message, nameof(filePath), e);
+        }
+        catch (UnauthorizedAccessException e)
+        {
+            throw new IOException(e.Message, e);
+        }
+        catch (NotSupportedException e)
+        {
+            throw new ArgumentException(e.Message, nameof(filePath), e);
+        }
+        catch (System.Security.SecurityException e)
+        {
+            throw new IOException(e.Message, e);
+        }
+        catch (PathTooLongException e)
+        {
+            throw new ArgumentException(e.Message, nameof(filePath), e);
+        }
+        catch (Exception e)
+        {
+            throw new IOException(e.Message, e);
+        }
+    }
+}
